43430d9584e3f4213aa7dd8f4be46d4fda60a825
Manuel Herdez <herdez@gmail.com>
2021-12-22 16:27:35 -0600

    probando dark theme

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -8,6 +8,7 @@ module.exports = {
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
+  theme: 'default-prefers-color-scheme',
   themeConfig: {
     editLinks: false,
     lastUpdated: false,
